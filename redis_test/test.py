a3e84fe182d25c4faecf568ad714f0cc303055c2
godkillok <fujianhaowawa@163.com>
2017-06-28 23:24:48 +0800

    initial

--- redis_test/test.py
+++ redis_test/test.py
@@ -4,10 +4,28 @@ import redis
 def run():
     t = redis
     r =t.Redis(host='localhost', port=6379)
-    r.set('bing', 'baz')
+    print(r.set('bing', 'baz'))
+    print(r.sadd('b2ing', 'baz'))
     r.rpush('rlist',1,2,3)
+    print(r.lrange('rlist', 0, -1) )
     print(r.get('bing'))
-    print(r.get('rlist'))
+    r.sadd('set-key', 'a', 'b', 'c')
+
+    print(r.smembers('set-key'))
+
+    a=r.hmset('user:013', {
+        'title': 'title',
+        'link': 'link',
+        'poster': 'user',
+        'time': 'now',
+        'votes': 1,
+    })
+    dic=r.hgetall('user:013')
+    for (k,v) in dic.items():
+        print(k,v)
+
+    print (r.hgetall('user:012'))
+    # print(r.get('rlist'))
 
 
 
